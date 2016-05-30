f032499193373595d50180e79dee746682597568
FR98 <franciscorosal98@gmail.com>
2016-05-30 08:41:16 -0600

    Agrego la funcion 7

---
+++ prob_7.py
@@ -0,0 +1,12 @@
+def prob_7():
+	multiplos = []
+	for i in range (0, 250):
+		cuatro = 4 * i
+		if cuatro < 1000:
+			multiplos.append(cuatro)
+		siete = 7 * i
+		if siete < 1000:
+			multiplos.append(siete)
+	return(multiplos)
+
+		
