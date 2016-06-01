26f5c0d72309e1003d0250371d802aa82c90e303
KarenCaballeros <anakarencaballeros@gmail.com>
2016-05-31 20:30:34 -0600

    arregle problema 6

--- prob_6.py
+++ prob_6.py
@@ -1,5 +1,8 @@
 def prob_6 (lista):
 	for a in range (len(lista)-1, 0, -1):
-		for b in xrange (a):
-			if lista[b] < lista[b+1]:
-				lista[b], lista[b+1]= lista[b+1], lista[b]
+		for b in range (a):
+			if lista[b] > lista[b+1]:
+				c= lista[b]
+				lista[b] = lista[b+1]
+				lista[b+1] = c
+	return lista
